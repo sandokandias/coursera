0f3bc31bef9159cae39b41373e8bc72ce45e39f3
Sandokan Dias <sandokandias@gmail.com>
2015-04-25 16:04:32 -0300

    week 1 - exercise 2

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -3,12 +3,18 @@ import common._
 
 object Main {
   def main(args: Array[String]) {
+
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+
+    println()
+    val text = args(0)
+    println(s"Parentheses Balancing: $text")
+    println(balance(text.toList))
   }
 
   /**
@@ -16,15 +22,24 @@ object Main {
    */
   def pascal(c: Int, r: Int): Int = {
     if (c == -1) 0
-    else if (c == r) 1
-    else if (c == 0) 1
+    else if (c == r || c== 0) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+
+    def balanceHelper(open: Int, close: Int, list: List[Char]): Boolean = {
+      if (list.isEmpty) open == close
+      else if (list.head == '(') balanceHelper(open + 1, close, list.tail)
+      else if (list.head == ')') balanceHelper(open, close + 1, list.tail)
+      else balanceHelper(open, close, list.tail)
+    }
+
+    balanceHelper(0, 0, chars)
+  }
 
   /**
    * Exercise 3
